4a91252d1ecf3880799fa58abdb81c4286cad6c6
Thahir Maheen <thahir@farabi.ae>
2017-02-27 18:19:12 +0400

    initial commit

---
+++ SampleDeleteTest/SampleDeleteTest/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SampleDeleteTest
+//
+//  Created by Thahir Maheen on 27-2-17.
+//  Copyright © 2017 Thahir Maheen. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
